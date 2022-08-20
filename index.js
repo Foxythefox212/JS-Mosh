4fbf1b0f156291e84073d64de017002a3b4148b1
Ruxandra Laura <Berbecutza1986@gmail.com>
2022-08-20 11:57:58 +0300

    Variable, Constant and Primitive Type.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 // This is my second JavaScript code!!!
 // JS-BASICS
 console.log("Hello World!");
+let name = "Ruxi";
+console.log(name);
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+let firstName = "Ruxandra"; // STRING LITERAL
+let age = 36; //NUMBER LITERAL
+let isApproved = true; //BOOLEAN LITERAL
+let secondName;
+let year = null;
+let selectedColor = null;
