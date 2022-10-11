7c9caa6abe16ca1c8b1e9351f2d0147db7b1a65d
Ruxandra Laura <Berbecutza1986@gmail.com>
2022-10-11 16:14:35 +0300

    Cateva coduri sa le am la nevoie

--- mdn.js
+++ mdn.js
@@ -154,10 +154,38 @@ let age = prompt("What is your age?", 18);
 let welcome =
   age < 18
     ? function () {
-        alert("Hello!");
-      }
+      alert("Hello!");
+    }
     : function () {
-        alert("Greetings!");
-      };
+      alert("Greetings!");
+    };
 
 welcome(); // ok now
+
+// Joc Ghiceste Numarul
+/* console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value;
+document.querySelector('.guess').value = 23; */
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;//Face nr aleatorii de la 1 la 20
+document.querySelector('.number').textContent = secretNumber;//Apare nr-ul secret
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number((document.querySelector('.guess').value));
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔No number!'
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈Too high!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉Too low!';
+  }
+});
